3b181e524aa32bdbfe77ada25c7bd6fba628b1cb
杨振炜 <20377408@buaa.edu.cn>
2023-02-27 13:13:39 +0800

    finish lab0

--- ray/sh_test2/search.sh
+++ ray/sh_test2/search.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 #First you can use grep (-n) to find the number of lines of string.
 #Then you can use awk to separate the answer.
+
+src=$1
+str=$2
+dst=$3
+
+grep -n $str < $src | sed -n -e 's/\:.*//gp' > $dst
