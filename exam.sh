1dda535269e3adfd506d6d906ca3297225a3aae5
Snak3s <1289203637@qq.com>
2023-03-06 19:15:19 +0800

    please read the statements carefully

--- exam.sh
+++ exam.sh
@@ -3,9 +3,10 @@ mkdir mydir
 chmod 777 mydir
 echo 2023 > myfile
 mv moveme mydir
-cp copyme mydir
+cp copyme mydir/copied
 cat readme
 gcc bad.c 2> err.txt
+mkdir gen
 n=10
 if [ $# -eq 1 ];
 then
@@ -14,6 +15,6 @@ fi
 i=1
 while [ $i -le $n ];
 do
-	touch $i.txt
+	touch gen/$i.txt
 	i=$(( $i + 1 ))
 done
