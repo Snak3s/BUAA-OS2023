f40189c9ae125a1f8c97182271cfd2ebe4797a8c
杨振炜 <20377408@buaa.edu.cn>
2023-04-24 19:46:11 +0800

    finish lab 4-1 extra

--- user/lib/ipc.c
+++ user/lib/ipc.c
@@ -35,5 +35,35 @@ u_int ipc_recv(u_int *whom, void *dstva, u_int *perm) {
 		*perm = env->env_ipc_perm;
 	}
 
+	/*
+	if (env->env_ipc_broadcast & IPC_BROADCAST) {
+		u_int va = dstva;
+		if (!(env->env_ipc_broadcast & IPC_PAGE_SENT)) {
+			va = 0;
+		}
+		ipc_broadcast(env->env_ipc_value, va, env->env_ipc_perm);
+	}
+	*/
+
 	return env->env_ipc_value;
+}
+
+void ipc_broadcast(u_int val, void *srcva, u_int perm) {
+	int sent[NENV];
+	for (int i = 0; i < NENV; i++) {
+		sent[i] = 0;
+	}
+	sent[ENVX(env->env_id)] = env->env_id;
+	int cnt = 1;
+	while (cnt) {
+		cnt = 0;
+		for (int i = 0; i < NENV; i++) {
+			if (envs[i].env_status != ENV_FREE &&
+			    sent[ENVX(envs[i].env_parent_id)] == envs[i].env_parent_id) {
+				cnt++;
+				sent[ENVX(envs[i].env_id)] = envs[i].env_id;
+				ipc_send(envs[i].env_id, val, srcva, perm | IPC_BROADCAST);
+			}
+		}
+	}
 }
