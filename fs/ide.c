00aea235a5fe0bad4761ec27b5acc86fc5d5988e
杨振炜 <20377408@buaa.edu.cn>
2023-04-27 03:44:17 +0800

    finish lab5

--- fs/ide.c
+++ fs/ide.c
@@ -32,7 +32,14 @@ void ide_read(u_int diskno, u_int secno, void *dst, u_int nsecs) {
 	for (u_int off = 0; begin + off < end; off += BY2SECT) {
 		uint32_t temp = diskno;
 		/* Exercise 5.3: Your code here. (1/2) */
-
+		syscall_write_dev(&temp, DEV_DISK_ADDRESS | DEV_DISK_ID, sizeof(uint32_t));
+		temp = begin + off;
+		syscall_write_dev(&temp, DEV_DISK_ADDRESS | DEV_DISK_OFFSET, sizeof(uint32_t));
+		temp = DEV_DISK_OPERATION_READ;
+		syscall_write_dev(&temp, DEV_DISK_ADDRESS | DEV_DISK_START_OPERATION, sizeof(uint32_t));
+		syscall_read_dev(&temp, DEV_DISK_ADDRESS | DEV_DISK_STATUS, sizeof(uint32_t));
+		panic_on(temp == 0);
+		syscall_read_dev(dst + off, DEV_DISK_ADDRESS | DEV_DISK_BUFFER, BY2SECT);
 	}
 }
 
@@ -59,6 +66,13 @@ void ide_write(u_int diskno, u_int secno, void *src, u_int nsecs) {
 	for (u_int off = 0; begin + off < end; off += BY2SECT) {
 		uint32_t temp = diskno;
 		/* Exercise 5.3: Your code here. (2/2) */
-
+		syscall_write_dev(&temp, DEV_DISK_ADDRESS | DEV_DISK_ID, sizeof(uint32_t));
+		temp = begin + off;
+		syscall_write_dev(&temp, DEV_DISK_ADDRESS | DEV_DISK_OFFSET, sizeof(uint32_t));
+		syscall_write_dev(src + off, DEV_DISK_ADDRESS | DEV_DISK_BUFFER, BY2SECT);
+		temp = DEV_DISK_OPERATION_WRITE;
+		syscall_write_dev(&temp, DEV_DISK_ADDRESS | DEV_DISK_START_OPERATION, sizeof(uint32_t));
+		syscall_read_dev(&temp, DEV_DISK_ADDRESS | DEV_DISK_STATUS, sizeof(uint32_t));
+		panic_on(temp == 0);
 	}
 }
