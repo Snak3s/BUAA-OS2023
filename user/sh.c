ffc8ce8b47c3409f50b5fa32180371be4dbd6aaf
杨振炜 <20377408@buaa.edu.cn>
2023-05-18 16:07:48 +0800

    finish lab6

--- user/sh.c
+++ user/sh.c
@@ -88,8 +88,12 @@ int parsecmd(char **argv, int *rightpipe) {
 			}
 			// Open 't' for reading, dup it onto fd 0, and then close the original fd.
 			/* Exercise 6.5: Your code here. (1/3) */
+			fd = open(t, O_RDONLY);
+			if ((r = dup(fd, 0)) < 0 || (r = close(fd)) < 0) {
+			       exit();
+			}	       
 
-			user_panic("< redirection not implemented");
+			// user_panic("< redirection not implemented");
 
 			break;
 		case '>':
@@ -99,8 +103,12 @@ int parsecmd(char **argv, int *rightpipe) {
 			}
 			// Open 't' for writing, dup it onto fd 1, and then close the original fd.
 			/* Exercise 6.5: Your code here. (2/3) */
+			fd = open(t, O_WRONLY);
+			if ((r = dup(fd, 1)) < 0 || (r = close(fd)) < 0) {
+				exit();
+			}
 
-			user_panic("> redirection not implemented");
+			// user_panic("> redirection not implemented");
 
 			break;
 		case '|':;
@@ -121,8 +129,20 @@ int parsecmd(char **argv, int *rightpipe) {
 			 */
 			int p[2];
 			/* Exercise 6.5: Your code here. (3/3) */
+			pipe(p);
+			*rightpipe = fork();
+			if (*rightpipe == 0) {
+				dup(p[0], 0);
+				close(p[0]);
+				close(p[1]);
+				return parsecmd(argv, rightpipe);
+			}
+			dup(p[1], 1);
+			close(p[1]);
+			close(p[0]);
+			return argc;
 
-			user_panic("| not implemented");
+			// user_panic("| not implemented");
 
 			break;
 		}
